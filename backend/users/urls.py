1a7002c67b83d9e4329f058d0c93d5b62ebe9ee7
mutuwa99 <mutuwanoble@MUTUWAs-MacBook-Air.local>
2024-03-28 16:11:11 +0200

    try 1

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('api/auth/login', views.welcome, name="api/auth/login"),
-    path('api/tickets/all', views.allticket, name="api/tickets/all"),
+    path('api/tickets/all', views.all_tickets, name="api/tickets/all"),
     path('api/tickets/delete/<int:id>/', views.delete_ticket, name="api/tickets/delete"),
     path('api/tickets/view/<int:id>/', views.view_ticket, name="api/tickets/view"),
     path('api/tickets/edit/<int:id>/', views.view_ticket, name="api/tickets/edit"),
